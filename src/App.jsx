aad136850a99f847c553f78b412786f6e76eb0fe
jxpsert <jasperdgp@outlook.com>
2023-07-05 14:48:27 +0200

    made it work

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,78 @@
 import { useState, useEffect } from "react";
-import "./App.css";
 import api from "./api";
+import ReactMarkdown from "react-markdown";
 
-async function App() {
-  let [posts, setPosts] = useState(null);
+function App() {
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     api.getAllPosts().then((data) => {
       console.log(data);
       setPosts(data);
     });
-  });
+  }, []);
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <ul>{posts}</ul>
-      </div>
+      <h1>Devlog</h1>
+      <ul>
+        {posts.map((post) => {
+          const months = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+          ];
+          const date = new Date(post.publishedAt);
+          const formattedDate = `${
+            months[date.getMonth()]
+          } ${date.getDate()}, ${date.getFullYear()} ${date
+            .getHours()
+            .toString()
+            .padStart(2, "0")}:${date
+            .getMinutes()
+            .toString()
+            .padStart(2, "0")}`;
+          return (
+            <li key={post.slug.current}>
+              <div className="post">
+                <h2 className="nbm">{post.title}</h2>
+                <span>
+                  by {post.author.name} &middot; {formattedDate}
+                </span>
+                <br></br>
+                <span>
+                  {post.tags.map((tag) => {
+                    return (
+                      <span
+                        key={tag.title}
+                        className="pill"
+                        style={{
+                          backgroundColor: tag.colour,
+                          color: tag.textcolour,
+                        }}
+                      >
+                        {tag.title}
+                      </span>
+                    );
+                  })}
+                </span>
+                <p>
+                  <ReactMarkdown>{post.body}</ReactMarkdown>
+                </p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export default {
   urls: {
     ALL_POSTS:
-      "https://l6kdrw9p.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%27post%27%5D+%7B%0A++title%2C+publishedAt%2C+slug%7Bcurrent%7D%2C+author+-%3E%7Bname%7D%2C+tags%5B%5D-%3E%7Btitle%2C+colour%7D%0A%7D",
+      "https://l6kdrw9p.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%27post%27%5D+%7B%0A++title%2C+body%2C+publishedAt%2C+slug%7Bcurrent%7D%2C+author+-%3E%7Bname%7D%2C+tags%5B%5D-%3E%7Btitle%2C+colour%2C+textcolour%7D%0A%7D+%7C+order%28publishedAt+desc%29&perspective=published",
     POST_BY_SLUG:
       "https://l6kdrw9p.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%27post%27+%26%26+slug+%3D%3D+%27REPLACE_WITH_SLUG%27%5D+%7B%0A++title%2C+publishedAt%2C+slug%7Bcurrent%7D%2C+author+-%3E%7Bname%7D%2C+tags%5B%5D-%3E%7Btitle%2C+colour%7D%0A%7D",
   },
